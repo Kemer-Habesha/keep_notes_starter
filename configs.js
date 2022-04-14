b679656e83dcd636f1035fa2e660872153cac5a7
NebyuKemer <nebyu@kemercode.com>
2022-04-13 21:39:51 -0500

    Comments added to explain the differences between module.exports and exports

--- configs.js
+++ configs.js
@@ -14,3 +14,17 @@ module.exports = {
     local: "",
   },
 };
+
+// module.exports.env = process.env.NODE_ENV;
+// module.exports.db = {
+//   remote: process.env.DB_REMOTE,
+//   local: "",
+// };
+
+// const negasi = {name: "negasi"}
+// negasi.age = 26;
+// #123HFI
+
+// const newNegasi = {name:"negasi",age:26}
+// negasi = newNegasi 
+// #12FGYU
